d4c748f97fdb9b74302feb665382b0053fab872c
David Alvarez <aalvag@live.com>
2022-02-17 12:11:07 -0300

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Colors from "../constants/Colors";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#000",
+      }}
+    >
+      <Text style={{ color: Colors.light.tint }}>
+        ply dummy text of the printing and typesetting industry
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
